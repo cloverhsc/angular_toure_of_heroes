0155f2391a0d18009508d4376d4ab22035652a40
clover <clover.hsc@gmail.com>
2016-10-30 02:15:22 +0800

    Chapter6 service

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Hero } from "./hero";
+import {HeroService} from "./hero.service";
 
-export class Hero {
-    id: number;
-    name: string;
-}
 
 @Component({
   selector: "my-app",
@@ -16,14 +14,7 @@ export class Hero {
             <span class="badge">{{hero.id}}</span>{{hero.name}}
         </li>
     </ul>
-    <div *ngIf="selectedHero">
-        <h2>{{selectedHero.name}} details! </h2>
-        <div><label>id: </label>{{selectedHero.id}}</div>
-        <div>
-            <label>name:</label>
-            <input [(ngModel)] = "selectedHero.name" placeholder="name"/>
-        </div>
-    </div>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
     `,
   styles: [`
       .selected {
@@ -73,27 +64,26 @@ export class Hero {
         margin-right: .8em;
         border-radius: 4px 0 0 4px;
       }
-  `]
+  `],
+  providers: [HeroService]
 })
 
 export class AppComponent {
     title = "Tour of Heroes";
     selectedHero: Hero;
-    heroes = HEROES;
+    heroes: Hero[];
+
+    constructor(private heroService: HeroService) { };
+
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
+    };
+
+    getHeroes(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+
+    ngOnInit(): void {
+        this.getHeroes();
     }
 }
-
-const HEROES: Hero[] = [
-    {id: 11, name: "Mr. Nice"},
-    {id: 12, name: "Narco"},
-    {id: 13, name: "Bombasto"},
-    {id: 14, name: "Celeritas"},
-    {id: 15, name: "Magneta"},
-    {id: 16, name: "RubberMan"},
-    {id: 17, name: "Dynama"},
-    {id: 18, name: "Dr IQ"},
-    {id: 19, name: "Magma"},
-    {id: 20, name: "Tornado"}
-];
